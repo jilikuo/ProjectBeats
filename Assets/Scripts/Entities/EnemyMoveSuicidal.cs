afc1ca19c39b62f012642d1d83dd5918e1aca6dd
jilikuo <joaovitorbariri@hotmail.com>
2024-08-03 21:30:47 -0300

    adding movement to script

--- Assets/Scripts/Entities/EnemyMoveSuicidal.cs
+++ Assets/Scripts/Entities/EnemyMoveSuicidal.cs
@@ -1,10 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EnemyMoveSuicidal : MonoBehaviour
 {
-    public readonly GameObject target = GameObject.FindGameObjectWithTag("Player");
+    private GameObject target;
+    private Transform playerPos;
 
+    private EntityStats stats;
+    private float speed;
 
+    void Start()
+    {
+        if (target == null)
+        {
+            target = GameObject.FindGameObjectWithTag("Player");
+        }
+
+        stats = GetComponent<EntityStats>();
+
+        playerPos = target.transform;
+
+        speed = stats.CalculateMaxSpeed();
+    }
+
+    void FixedUpdate()
+    {
+
+        Vector3 direction = playerPos.position - transform.position;
+        direction.Normalize();
+
+        transform.position += direction * speed * Time.fixedDeltaTime;
+    }
 }
