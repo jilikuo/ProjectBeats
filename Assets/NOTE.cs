eab865ac6a5e105e787aba85214cba35112b3756
jilikuo <joaovitorbariri@hotmail.com>
2024-08-23 08:48:28 -0300

    Cleaning up Assets Folder

---
+++ Assets/NOTE.cs
@@ -0,0 +1,17 @@
+/* as of 23/08/23 this project uses the following free assets:
+
+###########################################################################
+---------------------------------------------------------------------------
+################  2D Casual UI HD by MiMu Studio ##########################
+https://assetstore.unity.com/packages/2d/gui/icons/2d-casual-ui-hd-82080
+https://assetstore.unity.com/publishers/20972
+mimustudio.com/
+---------------------------------------------------------------------------
+###########################################################################
+---------------------------------------------------------------------------
+######################  Gun Icons Package  ################################
+https://assetstore.unity.com/packages/2d/gui/icons/gun-icons-package-287096
+https://assetstore.unity.com/publishers/100410
+sites.google.com/view/navid-tabatabaee-far
+###########################################################################
+*/
