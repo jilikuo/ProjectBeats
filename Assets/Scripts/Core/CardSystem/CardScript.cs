162fe2fb9d2344a914192adf29b51e2139dfa36a
jilikuo <joaovitorbariri@hotmail.com>
2024-08-26 14:28:34 -0300

    Finishing Card System 1.0

--- Assets/Scripts/Core/CardSystem/CardScript.cs
+++ Assets/Scripts/Core/CardSystem/CardScript.cs
@@ -16,10 +16,19 @@ namespace Jili.StatSystem.CardSystem
         private Image cardRarityField;
         private Image cardFaceField;
 
+        // level stars
+        private Image starOne;
+        private Image starTwo;
+        private Image starThree;
+        private Image starFour;
+        private Image starFive;
+
         private void Awake()
         {
             cardNameField = transform.Find("NameBox/NameText")?.GetComponent<TextMeshProUGUI>();
             cardRarityField = transform.Find("NameBox/RarityMedal")?.GetComponent<Image>();
+            cardLevelField = transform.Find("LevelBox");
+            cardFaceField = transform.Find("Background/WeaponIcon")?.GetComponent<Image>();
 
             if (cardNameField == null)
             {
@@ -27,7 +36,44 @@ namespace Jili.StatSystem.CardSystem
             }
 
             cardNameField.text = cardData.cardName;
+            cardRarityField.color = cardData.RarityColor;
             cardRarityField.sprite = cardData.RaritySprite;
+            cardFaceField.sprite = cardData.cardFace;
+            LoadCardLevel();
+        }
+
+        private void LoadCardLevel()
+        {
+            if ((int)cardData.cardLevel >= 1)
+            {
+                starOne = cardLevelField.Find("StarOne")?.GetComponent<Image>();
+                starOne.color = cardData.LevelColor;
+                starOne.sprite = cardData.LevelSprite;
+            }
+            if ((int)cardData.cardLevel >= 2)
+            {
+                starTwo = cardLevelField.Find("StarTwo")?.GetComponent<Image>();
+                starTwo.color = cardData.LevelColor;
+                starTwo.sprite = cardData.LevelSprite;
+            }
+            if ((int)cardData.cardLevel >= 3)
+            {
+                starThree = cardLevelField.Find("StarThree")?.GetComponent<Image>();
+                starThree.color = cardData.LevelColor;
+                starThree.sprite = cardData.LevelSprite;
+            }
+            if ((int)cardData.cardLevel >= 4)
+            {
+                starFour = cardLevelField.Find("StarFour")?.GetComponent<Image>();
+                starFour.color = cardData.LevelColor;
+                starFour.sprite = cardData.LevelSprite;
+            }
+            if ((int)cardData.cardLevel >= 5)
+            {
+                starFive = cardLevelField.Find("StarFive")?.GetComponent<Image>();
+                starFive.color = cardData.LevelColor;
+                starFive.sprite = cardData.LevelSprite;
+            }
         }
     }
 }
--- Assets/Scripts/Core/CardSystem/ScriptableCardData.cs
+++ Assets/Scripts/Core/CardSystem/ScriptableCardData.cs
@@ -15,11 +15,22 @@ public class ScriptableCardData : ScriptableObject
     public Sprite cardFace;
 
     public Sprite RaritySprite { get; private set; }
+    public Sprite LevelSprite { get; private set;}
+    public Color RarityColor { get; private set; }    
+    public Color LevelColor { get; private set; }
+
 
     private void OnEnable()
     {
+        // Load the GUI atlas and figure out the rarity sprite
+        // also loads the level Star.
         Addressables.LoadAssetAsync<Sprite[]>(guiAtlasAddress).Completed += OnSpriteLoaded;
 
+        // Set the rarity color based on the rarity
+        // Should be temporary, once we have assets for each rarity,
+        // we should use the sprite instead
+        CalculateRarityColor();
+        CalculateLevelColor();
     }
 
     private void OnSpriteLoaded(AsyncOperationHandle<Sprite[]> handle)
@@ -28,6 +39,7 @@ public class ScriptableCardData : ScriptableObject
         {
             Sprite[] guiTable = handle.Result;
             RaritySprite = GetRaritySprite(guiTable);
+            LevelSprite = GetLevelStarSprite(guiTable);
         }
         else
         {
@@ -55,19 +67,71 @@ public class ScriptableCardData : ScriptableObject
         switch (cardRarity)
         {
             case CardRarity.Inferior:
-                return "GUI_1";
-            case CardRarity.Common:
-                return "GUI_2";
-            case CardRarity.Rare:
-                return "GUI_3";
-            case CardRarity.Legendary:
-                return "GUI_4";
-            case CardRarity.Mythic:
-                return "GUI_5";
-            case CardRarity.Godly:
                 return "GUI_6";
+            case CardRarity.Common:
+                return "GUI_6";
+            case CardRarity.Rare:
+                return "GUI_5";
+            case CardRarity.Legendary:
+                return "GUI_5";
+            case CardRarity.Mythic:
+                return "GUI_4";
+            case CardRarity.Godly:
+                return "GUI_4";
             default:
-                return "GUI_1";
+                return "GUI_5";
+        }
+    }
+
+    private void CalculateRarityColor()
+    {
+        switch (cardRarity)
+        {
+            case CardRarity.Inferior:
+                RarityColor = new(0.4f, 0.4f, 0.4f, 1f);
+                break;
+            default:
+                RarityColor = new(1f, 1f, 1f, 1f);
+                break;
+        }
+    }
+
+    private Sprite GetLevelStarSprite(Sprite[] sprites)
+    {
+        string spriteName = GetSpriteNameBasedOnLevel();
+
+        foreach (var sprite in sprites)
+        {
+            if (sprite.name == spriteName)
+            {
+                return sprite;
+            }
+        }
+
+        throw new System.Exception("Level star sprite not found for " + cardName + " card");
+    }
+
+    private string GetSpriteNameBasedOnLevel()
+    {
+        switch (cardLevel)
+        {
+            case CardLevel.Max:
+                return "GUI_45";
+            default:
+                return "GUI_24";
+        }
+    }
+
+    private void CalculateLevelColor()
+    {
+        switch (cardLevel)
+        {
+            case CardLevel.Max:
+                LevelColor = new(0.5f, 0f, 0.6f, 1f);
+                break;
+            default:
+                LevelColor = new(1f, 1f, 1f, 1f);
+                break;
         }
     }
 }
